6f6dcf64967b2aa5fa0fcaa81c597aaf4e560ec9
evenRao <bhavyadeepjayprakash.rao035@myci.csuci.edu>
2025-03-09 17:00:16 -0700

    Remove spmatching solo file

--- spmatching/app/src/main/java/com/edkura/spmatching/StudyPartnerRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.edkura.spmatching
-
-data class StudyPartnerRequest(
-    var id: String = "",
-    var senderId: String = "",
-    var receiverId: String = "",
-    var status: String = "pending"
-)
