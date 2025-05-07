abf3ebd3f681739837fcdd810ce6f79d594ea617
JiankaiZh <jiankai.zhong266@myci.csuci.edu>
2025-05-06 18:18:08 -0700

    5/6

--- EdKura/app/src/main/java/com/example/edkura/chat/AllChatsActivity.kt
+++ EdKura/app/src/main/java/com/example/edkura/chat/AllChatsActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
